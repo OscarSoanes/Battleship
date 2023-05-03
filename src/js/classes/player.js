e9231d9ca314f6906e0c3b39eb8f536c1f136000
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-03 16:48:55 +0100

    Game now generates ships for user

--- src/js/classes/player.js
+++ src/js/classes/player.js
@@ -1,9 +1,22 @@
 import { Gameboard } from "./gameboard";
+import { Ship } from "./ship";
 
 export class Player {
     constructor(name) {
         this.name = name;
         this.prevMoves = [];
         this.gameboard = new Gameboard;
+        this.ships = generateShips()
     }
+}
+
+function generateShips() {
+    const lengths = [2, 3, 3, 4, 5];
+    const ships = []
+
+    lengths.forEach(length => {
+        ships.push(new Ship(length))
+    })
+
+    return ships;
 }
--- src/js/tests/player.test.js
+++ src/js/tests/player.test.js
@@ -1,11 +1,13 @@
 import { Gameboard } from '../classes/gameboard';
 import { Player } from '../classes/player'
+import { Ship } from '../classes/ship';
 
 describe("Testing the player", () => {
     test("Creating a player", () => {
         const player = new Player("test");
         const gameboard = new Gameboard();
+        const ships = [new Ship(2), new Ship(3), new Ship(3), new Ship(4), new Ship(5)]
 
-        expect(player).toEqual({name: "test", prevMoves: [], gameboard: gameboard})
+        expect(player).toEqual({name: "test", prevMoves: [], gameboard: gameboard, ships: ships})
     })
 })
