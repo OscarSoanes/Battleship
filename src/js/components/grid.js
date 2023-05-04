9c2c2791e3b98edda24c271f952da530839cdac4
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-04 16:16:48 +0100

    Start game, creates a grid with the ability to hover

--- src/js/components/grid.js
+++ src/js/components/grid.js
@@ -8,14 +8,14 @@ export function createGrid(message) {
   const grid = document.createElement("div");
   grid.classList.add("grid");
 
-  for (let x = 0; x < 10; x++) {
+  for (let y = 0; y < 10; y++) {
     const row = document.createElement("div");
     row.classList.add("row");
 
-    for (let y = 0; y < 10; y++) {
+    for (let x = 0; x < 10; x++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
-      cell.setAttribute("location", `{x: ${x}, y: ${y}}`);
+      cell.setAttribute("location", `{"x": "${x}", "y": "${y}"}`);
 
       row.appendChild(cell);
     }
--- src/js/game/startGame.js
+++ src/js/game/startGame.js
@@ -1,5 +1,97 @@
 import { createGame } from "../components/createGame";
+import { Player } from "../classes/player";
+import { getOrientation } from "./getAxis";
 
-export function startGame() {
+export function startGame(name) {
   createGame();
+
+  const player = new Player(name);
+
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) =>
+    cell.addEventListener("mouseover", () => {
+      removeHoverClassFromCell(cells);
+      gridEventListener(cell, player.ships[0], player);
+    })
+  );
+}
+
+/**
+ *
+ * @param {HTMLDivElement} cell
+ * @param {*} ship
+ */
+function gridEventListener(cell, ship, player) {
+  const length = ship.length;
+  const axis = getOrientation();
+
+  const position = JSON.parse(cell.getAttribute("location"));
+
+  const shipLengthArr = new Array(ship.length).fill(" ");
+  let positionToInt = { x: parseInt(position.x), y: parseInt(position.y) };
+
+  if (
+    player.gameboard.checkOutOfBounds(shipLengthArr, positionToInt, axis) ===
+    "collision"
+  ) {
+    positionToInt = { x: parseInt(position.x), y: parseInt(position.y) };
+    goUntilError(positionToInt, axis, shipLengthArr, player);
+    return;
+  }
+
+  hoverOnShip(length, axis, position);
+}
+
+/**
+ *
+ * @param {NodeListOf<Element>} cells
+ */
+function removeHoverClassFromCell(cells) {
+  cells.forEach((cell) => {
+    cell.classList.remove("hover-effect");
+    cell.classList.remove("cell-error");
+  });
+}
+
+function hoverOnShip(length, axis, position) {
+  for (let index = 0; index < length; index++) {
+    // console.log(`[location=\'{"x": "${position.x}", "y": "${position.y}"}\']`);
+    const element = document.querySelector(
+      `[location=\'{"x": "${position.x}", "y": "${position.y}"}\']`
+    );
+
+    element.classList.add("hover-effect");
+
+    if (axis === "vertical") {
+      position.y++;
+    } else {
+      position.x++;
+    }
+  }
+}
+
+function goUntilError(position, axis, lengthArr, player) {
+  lengthArr.every(() => {
+    const startingPos = JSON.parse(JSON.stringify(position));
+
+    if (
+      player.gameboard.checkOutOfBounds([" "], position, axis) === "collision"
+    ) {
+      return false;
+    }
+
+    const element = document.querySelector(
+      `[location=\'{"x": "${startingPos.x}", "y": "${startingPos.y}"}\']`
+    );
+    element.classList.add("cell-error");
+
+    if (axis === "vertical") {
+      startingPos.y++;
+    } else {
+      startingPos.x++;
+    }
+
+    position = startingPos;
+    return true;
+  });
 }
