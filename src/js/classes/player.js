773ccec125f660a0de66919c47fde761db11086b
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-04 18:39:37 +0100

    Prettier

--- src/js/classes/player.js
+++ src/js/classes/player.js
@@ -2,59 +2,59 @@ import { Gameboard } from "./gameboard";
 import { Ship } from "./ship";
 
 export class Player {
-    constructor(name) {
-        this.name = name;
-        this.prevMoves = [];
-        this.gameboard = new Gameboard();
-        this.ships = generateShips()
+  constructor(name) {
+    this.name = name;
+    this.prevMoves = [];
+    this.gameboard = new Gameboard();
+    this.ships = generateShips();
+  }
+
+  placeShip(x, y, axis) {
+    const nextShip = this.ships.shift();
+
+    const result = this.gameboard.placeShip({ x: x, y: y }, axis, nextShip);
+
+    if (result === "collision") {
+      this.ships.unshift(nextShip);
     }
-    
-    placeShip(x, y, axis) {
-        const nextShip = this.ships.shift();
+  }
 
-        const result = this.gameboard.placeShip({x: x, y: y}, axis, nextShip)
-
-        if (result === "collision") {
-            this.ships.unshift(nextShip);
-        }
+  autoPlaceShips() {
+    const axis = ["horizontal", "vertical"];
+    while (this.ships.length != 0) {
+      const x = Math.round(Math.random() * 9);
+      const y = Math.round(Math.random() * 9);
+      const thisAxis = axis[Math.round(Math.random())];
+      this.placeShip(x, y, thisAxis);
     }
+  }
 
-    autoPlaceShips() {
-        const axis = ["horizontal", "vertical"];
-        while (this.ships.length != 0) {
-            const x = Math.round(Math.random() * 9); 
-            const y = Math.round(Math.random() * 9); 
-            const thisAxis = axis[Math.round(Math.random())]
-            this.placeShip(x, y, thisAxis);
-        }
-    }
-
-    incomingAttack(x, y) {
-        if (this.prevMoves.some(coords => coords.x === x && coords.y === y)) {
-            return "already-found";
-        }
-
-        this.prevMoves.push({x: x, y: y});
-        return this.gameboard.recieveAttack({x: x, y: y});
+  incomingAttack(x, y) {
+    if (this.prevMoves.some((coords) => coords.x === x && coords.y === y)) {
+      return "already-found";
     }
 
-    randomAttack() {
-        const x = Math.round(Math.random() * 9); 
-        const y = Math.round(Math.random() * 9); 
+    this.prevMoves.push({ x: x, y: y });
+    return this.gameboard.recieveAttack({ x: x, y: y });
+  }
 
-        if (this.incomingAttack(x, y) === "already-found") {
-            this.randomAttack();
-        }
+  randomAttack() {
+    const x = Math.round(Math.random() * 9);
+    const y = Math.round(Math.random() * 9);
+
+    if (this.incomingAttack(x, y) === "already-found") {
+      this.randomAttack();
     }
+  }
 }
 
 function generateShips() {
-    const lengths = [2, 3, 3, 4, 5];
-    const ships = []
+  const lengths = [2, 3, 3, 4, 5];
+  const ships = [];
 
-    lengths.forEach(length => {
-        ships.push(new Ship(length))
-    })
+  lengths.forEach((length) => {
+    ships.push(new Ship(length));
+  });
 
-    return ships;
+  return ships;
 }
